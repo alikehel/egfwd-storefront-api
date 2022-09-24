41ae18eb3eff9fabe78169ca2f61eb3876b610e9
Ali Kehel <ali.saber.27.2@gmail.com>
2022-09-24 14:50:00 +0200

    fix typo

--- migrations/sqls/20220922193243-orders-products-up.sql
+++ migrations/sqls/20220922193243-orders-products-up.sql
@@ -4,6 +4,6 @@ CREATE TABLE
     orders_products  (
     id SERIAL PRIMARY KEY,
     orderid INT REFERENCES orders(id) NOT NULL,
-    prductid INT REFERENCES products(id) NOT NULL,
+    productid INT REFERENCES products(id) NOT NULL,
     quantity INT NOT NULL
 );
